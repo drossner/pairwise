4e4a8eca189629e3c52110ba3048567d8ff20c84
czeitler2 <christian.zeitler.2@hof-university.de>
2020-04-07 09:40:11 +0200

    clean up code

--- src/main/resources/vue/views/simulation-view.vue
+++ src/main/resources/vue/views/simulation-view.vue
@@ -1,7 +1,7 @@
 <template id="simulation-view">
     <div class="row spatial-row mt-2 position-sticky">
         <div class="col col-12" id="canvasContainer" ref="canvasContainer"></div>
-        <div class="position-absolute m-2"style="top: 0">
+        <div class="position-absolute m-2" style="top: 0">
             <b-collapse visible id="collapse-3">
                 <b-card>
                     simulation usage:<br/>
@@ -9,9 +9,13 @@
                     - press the left mouse button and move the node<br/>
                     - release the left mouse button and the moved node stays dynamic<br/>
                     drag-and-drop with right mouse button:<br/>
+                    - to fix the node at the mouse position, use ONLY right mouse button<br/>
                     - press the right mouse button and move the node<br/>
-                    - release the right mouse button and the moved item changes its color and is fixed at the mouse position<br/>
-                    - to change the type of the node from static back to dynamic click the right mouse button on the node.</b-card>
+                    - release the right mouse button and the moved item changes its color and is fixed at the mouse
+                    position<br/>
+                    - to change the type of the node from static back to dynamic click the right mouse button on the
+                    node
+                </b-card>
             </b-collapse>
             <b-button v-b-toggle.collapse-3 class="position-absolute m-2" variant="primary">Help</b-button>
         </div>
@@ -57,7 +61,6 @@
             }
         },
         mounted() {
-            //this.createPollCanvas(this.$refs.canvasContainer);
             this.resize();
         },
         created() {
@@ -96,11 +99,10 @@
                     self.ground = self.world.CreateBody(groundDef);
                     self.ground.CreateFixture(groundFixt);
 
-                    //fix factor of 10 like 100px = 10meter
+                    //fix factor of 50 like 500px = 50meter
                     let cam = 50;
                     //create bodies:
                     for (let i = 0; i < self.concepts.length; i++) {
-                        //self.concepts[i].rightClicked = false;
                         let kon = self.concepts[i].konvaNode;
                         let konPos = self.getGroupPos(kon);
                         self.groupPosition = konPos;
@@ -109,7 +111,6 @@
                         bodyDef.set_type(ph.b2_dynamicBody);
                         bodyDef.set_fixedRotation(true);
                         bodyDef.set_position(new ph.b2Vec2(konPos.x / cam, konPos.y / cam));
-                        //bodyDef.set_position(new ph.b2Vec2(konPos.x, konPos.y));
                         bodyDef.set_linearDamping(9);
                         bodyDef.set_angularDamping(0.9);
 
@@ -124,8 +125,6 @@
                         fixture.set_isSensor(false);
 
                         let body = self.world.CreateBody(bodyDef);
-                        //body.set_linearDamping(9);
-                        //body.set_angularDamping(0.9);
                         body.CreateFixture(fixture); //does not work..
                         self.concepts[i].box2dNode = body;
                     }
@@ -152,12 +151,13 @@
                     let interval = 35;
                     let origFrames = (1000 / (interval * self.speed));
                     let run = setInterval(request, interval);
+
                     function request() {
                         //update mouseJoint
-                        if(self.dragging){
+                        if (self.dragging) {
                             let pos = self.stage.getPointerPosition();
-                            //self.mouseJoint.SetTarget(new ph.b2Vec2(0, 0));
-                            self.mouseJoint.SetTarget(new ph.b2Vec2((((pos.x/self.stage.scaleX())/cam) - self.bodyPos.get_x()), (((pos.y/self.stage.scaleY())/cam) - self.bodyPos.get_y())));
+                            //pointer position - body pos to prevent an offset
+                            self.mouseJoint.SetTarget(new ph.b2Vec2((((pos.x / self.stage.scaleX()) / cam) - self.bodyPos.get_x()), (((pos.y / self.stage.scaleY()) / cam) - self.bodyPos.get_y())));
                         }
                         self.world.Step(1 / origFrames, 4, 4); // as in android, max iteration collision, max pos iteration
                         //move konva stuff..
@@ -197,9 +197,7 @@
                 else if (w >= 576 && w < 768) this.stage.scale({x: 0.7, y: 0.7});
                 else if (w >= 768 && w < 992) this.stage.scale({x: 0.9, y: 0.9});
                 else if (w >= 992) this.stage.scale({x: 1, y: 1});
-                //$.toast('Width: '+w);
                 this.stage.batchDraw();
-                //console.log(this.stage.scale());
             },
             createPollCanvas: function (containerID) {
                 const stage = new Konva.Stage({
@@ -210,7 +208,7 @@
                 this.stage = stage;
                 const mainLayer = new Konva.Layer();
                 const lineLayer = new Konva.Layer();
-                let concepts = this.concepts.map(concept => concept.name); //todo
+                let concepts = this.concepts.map(concept => concept.name);
                 let nodes = [];
                 let self = this;
 
@@ -228,51 +226,41 @@
                     //drag-and-drop with right mouse button
                     //set node body to static und changes color to red
                     nodes[i].on('click', function (e) {
-                       if (e.evt.button === 2){
-                           if (nodes[i].rightClicked === false){
-                               nodes[i].rightClicked = true;
-                               nodes[i].getChildren()[0].fill('red');
-                               self.concepts[i].box2dNode.SetType(self.ph.b2_staticBody);
-                           }
-                           else {
-                               nodes[i].rightClicked = false;
-                               nodes[i].getChildren()[0].fill('white');
-                               self.concepts[i].box2dNode.SetType(self.ph.b2_dynamicBody);
-                           }
-                       }
-                        console.log(self.concepts[i].box2dNode.GetType());
+                        if (e.evt.button === 2) {
+                            if (nodes[i].rightClicked === false) {
+                                nodes[i].rightClicked = true;
+                                nodes[i].getChildren()[0].fill('red');
+                                self.concepts[i].box2dNode.SetType(self.ph.b2_staticBody);
+                            } else {
+                                nodes[i].rightClicked = false;
+                                nodes[i].getChildren()[0].fill('white');
+                                self.concepts[i].box2dNode.SetType(self.ph.b2_dynamicBody);
+                            }
+                        }
                     });
 
                     nodes[i].on('mousedown', function () {
                         self.clickedNode = nodes[i].textContent;
-                        //console.log("start drag");
-                        //let pos = nodes[i].getStage().getPointerPosition(); //pixel
                         let temp = self.concepts[i].box2dNode.GetPosition(); //physic
                         self.bodyPos = new self.ph.b2Vec2(temp.get_x(), temp.get_y());
-                        //self.concepts[i].box2dNode.SetLinearVelocity(vec);
                         //other
                         let body = self.concepts[i].box2dNode;
                         let jDef = new self.ph.b2MouseJointDef();
                         jDef.set_bodyA(self.ground);
-                        jDef.set_bodyB (body);
-                        //jDef.set_target(new self.ph.b2Vec2(pos.x/10, pos.y/10)); //todo: cam to member var
-                        //jDef.set_target(new self.ph.b2Vec2(1000, 1000)); //todo: cam to member var
+                        jDef.set_bodyB(body);
                         jDef.set_maxForce(10000);
                         jDef.set_frequencyHz(10);
                         jDef.set_dampingRatio(0.5);
                         self.mouseJoint = self.ph.castObject(self.world.CreateJoint(jDef), self.ph.b2MouseJoint);
-                        //self.mouseJoint.SetTarget(new self.ph.b2Vec2(100,100));
                         body.SetAwake(true);
-                        //console.log(self.world.GetJointCount());
                         self.dragging = true;
-                        for (let j = 0; j < self.distanceJoint.length; j ++) {
+                        for (let j = 0; j < self.distanceJoint.length; j++) {
                             self.world.DestroyJoint(self.distanceJoint[j]);
                         }
                         self.distanceJoint = [];
 
                     });
                 }
-                //console.info(concepts);
                 this.lines = this.createLines(nodes);
 
                 nodes.forEach(value => {
@@ -287,15 +275,13 @@
                 stage.add(mainLayer);
 
                 window.addEventListener("mouseup", function () {
-                    if(self.dragging === true){
-                        //console.log("stop drag");
+                    if (self.dragging === true) {
                         self.dragging = false;
                         self.world.DestroyJoint(self.mouseJoint);
 
                         for (let i = 0; i < self.distanceJointDef.length; i++) {
                             self.distanceJoint.push(self.world.CreateJoint(self.distanceJointDef[i]));
                         }
-
                         self.clickedNode = "";
                     }
                 })
@@ -306,11 +292,10 @@
                 for (let i = 0; i < nodeArr.length - 1; i++) {
                     for (let k = i + 1; k < nodeArr.length; k++) {
 
-
                         //line between i and k
                         let nk = nodeArr[k], ni = nodeArr[i];
-                        let iBox = this.getGroupBox(ni);
-                        let kBox = this.getGroupBox(nk);
+                        //let iBox = this.getGroupBox(ni);
+                        //let kBox = this.getGroupBox(nk);
                         let line = new Konva.Line({
                             points: [
                                 nk.x(), nk.y(),
@@ -345,7 +330,7 @@
                 return lineArr;
             },
             createNode: function (text, x, y) {
-                const width = 100, height = 60;
+                const height = 60;
                 const group = new Konva.Group({
                     x: x, y: y,
                     draggable: true,
@@ -366,24 +351,6 @@
                     fontSize: 12,
                     align: 'center'
                 });
-                /*
-                group.rightClicked = false;
-                group.on('click', function (e) {
-                    if (e.evt.button === 2){
-                        if (group.rightClicked === false){
-                            group.rightClicked = true;
-                            console.log(group.rightClicked + ' ' +  group.textContent);
-                            rect.fill('red');
-
-                        }
-                        else {
-                            group.rightClicked = false;
-                            console.log(group.rightClicked + ' ' +  group.textContent);
-                            rect.fill('white');
-                        }
-                    }
-                });
-                 */
 
                 group.add(rect);
                 group.add(content);
@@ -392,12 +359,6 @@
                 return group;
             },
             updateLine: function (nodeA, nodeB, line, idealDistance) {
-
-
-
-                //getParent
-                let aBox = this.getGroupBox(nodeA);
-                let bBox = this.getGroupBox(nodeB);
                 line.points(
                     [
                         nodeA.x(), nodeA.y(),
@@ -406,42 +367,60 @@
                 let fstA = line.points().slice(0, 2);
                 let sndA = line.points().slice(2, 4);
                 let length = Math.sqrt(Math.pow(fstA[0] - sndA[0], 2) + Math.pow(fstA[1] - sndA[1], 2));
-                //length = length*this.stage.scaleX();
-                let minStroke = 0.1;
-                let maxStroke = 80;
-                //1000 => 0.1
-                //60 => 80
-                let normedLength = Math.min(length, 1000);
-                normedLength = Math.max(normedLength, 60);
-                let normedStroke = (-0.03993403403) * normedLength + 40; //(2162/27); //Mathemagie (leicht verzaubert für 60..
-                let stroke = Math.min((1 / length) * 100, 50);
-                //line.strokeWidth(normedStroke/2); //1118 vertical px in 1000 * 500
                 line.strokeWidth(3); //1118 vertical px in 1000 * 500
                 //calc delta and stroke
-                //let delta = Math.min(Math.abs(length - idealDistance), this.maxDist); //delta max = max distance, delta min = 0
-                //let deltaGroup = Math.round(delta / this.distStep); // 0..9
                 let delta = Math.abs(length - idealDistance);
-                let deltaGroup = Math.round((delta / idealDistance)*10);
+                let deltaGroup = Math.round((delta / idealDistance) * 10);
                 let color = "lightgray";
                 let opac = 1;
                 switch (deltaGroup) {
-                    case 0: opac = 0.5; color = "#1AE600"; break;
-                    case 1: opac = 0.55; color = "#33CC00"; break;
-                    case 2: opac = 0.6; color = "#4DB300"; break;
-                    case 3: opac = 0.65; color = "#669900"; break;
-                    case 4: opac = 0.7; color = "#808000"; break;
-                    case 5: opac = 0.75; color = "#996600"; break;
-                    case 6: opac = 0.8; color = "#B34D00"; break;
-                    case 7: opac = 0.85; color = "#CC3300"; break;
-                    case 8: opac = 0.9; color = "#E61A00"; break;
-                    case 9: opac = 1.0; color = "#FF0000"; break;
-                    default: color = "#FF0000"
+                    case 0:
+                        opac = 0.5;
+                        color = "#1AE600";
+                        break;
+                    case 1:
+                        opac = 0.55;
+                        color = "#33CC00";
+                        break;
+                    case 2:
+                        opac = 0.6;
+                        color = "#4DB300";
+                        break;
+                    case 3:
+                        opac = 0.65;
+                        color = "#669900";
+                        break;
+                    case 4:
+                        opac = 0.7;
+                        color = "#808000";
+                        break;
+                    case 5:
+                        opac = 0.75;
+                        color = "#996600";
+                        break;
+                    case 6:
+                        opac = 0.8;
+                        color = "#B34D00";
+                        break;
+                    case 7:
+                        opac = 0.85;
+                        color = "#CC3300";
+                        break;
+                    case 8:
+                        opac = 0.9;
+                        color = "#E61A00";
+                        break;
+                    case 9:
+                        opac = 1.0;
+                        color = "#FF0000";
+                        break;
+                    default:
+                        color = "#FF0000"
                 }
 
                 if (this.clickedNode === nodeA.textContent || this.clickedNode === nodeB.textContent) {
                     opac = 1;
-                }
-                else if (this.clickedNode !== "") {
+                } else if (this.clickedNode !== "") {
                     color = "lightgray";
                     opac = 0.3;
                 }
