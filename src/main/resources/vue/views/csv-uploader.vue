88c6271f306dc40139087960352bfd0155c10822
czeitler2 <christian.zeitler.2@hof-university.de>
2020-05-07 10:51:21 +0200

    add info and submit button calculate average weight and number of entities

--- src/main/resources/vue/views/csv-uploader.vue
+++ src/main/resources/vue/views/csv-uploader.vue
@@ -2,15 +2,23 @@
     <div>
         <!-- accept only .csv other files can be added -->
         <b-form-file
+                ref="csv-input"
                 v-model="csvFile"
                 :state="Boolean(csvFile)"
                 accept=".csv"
                 placeholder="Choose CSV-File or drop it here.."
                 drop-placeholder="Drop CSV-File here.."
         ></b-form-file>
-        <p class="mt-3">Selected file: <b>{{ csvFile ? csvFile.name : '' }}</b></p>
-        <b-button class="mt-2" @click="csvFile = null">Clear</b-button>
-        <b-button class="mt-2" @click="csvSubmit()">Submit</b-button>
+        <!-- <p class="mt-3">Selected file: <b>{{ csvFile ? csvFile.name : '' }}</b></p> -->
+        <div>
+            <label>Entities: <b>{{ entities ? entities.length : 0 }}</b></label>
+        </div>
+        <div>
+            <label>Average weight: <b>{{ avgWeight ? avgWeight : 0 }}</b></label>
+        </div>
+        <b-button class="mt-2" @click="clearFiles()">Clear</b-button>
+        <b-button v-if="infoFlag" class="mt-2" @click="csvInfo()">Info</b-button>
+        <b-button v-else class="mt-2" @click="csvSubmit()">Submit</b-button>
     </div>
 </template>
 
@@ -21,16 +29,33 @@
             return {
                 csvFile: null,
                 fileInput: '',
-                fileInputAsJSON: {}
+                fileInputAsJSON: {},
+                entities: [],
+                sample: {},
+                rand: [],
+                avgWeight: 0,
+                infoFlag: true
             }
         },
         methods: {
+            clearFiles() {
+                this.$refs["csv-input"].reset();
+                this.csvFile = null;
+                this.fileInput= '';
+                this.fileInputAsJSON = {};
+                this.entities = [];
+                this.sample = {};
+                this.rand = [];
+                this.avgWeight = 0;
+                this.infoFlag = true;
+            },
+
             csvJSON(csv) {
                 let lines = csv.split("\n");
                 let result = [];
                 let headers = lines[0].split(";");
 
-                for (let i = 1; i < lines.length; i++) {
+                for (let i = 1; i < lines.length-1; i++) {
                     let obj = {};
                     let currentLine = lines[i].split(";");
                     for (let j = 0; j < headers.length; j++) {
@@ -40,22 +65,52 @@
                 }
                 return result;
             },
-            csvSubmit() {
-                let entities = [];
+
+            csvInfo() {
                 const reader = new FileReader();
-                let fileInputAsJSON = {};
+                let ctx = 0;
                 reader.onload = ev => {
                     this.fileInput = ev.target.result;
-                    fileInputAsJSON = this.csvJSON(this.fileInput);
+                    this.fileInputAsJSON = this.csvJSON(this.fileInput);
+
                     //iterate over fileInputAsJSON and save entities..
-                    /*for (let i = 0; i < fileInputAsJSON.length; i++) {
-                            if (entities.includes(fileInputAsJSON[i].Target)) entities.push(JSON.parse(fileInputAsJSON[i].Target));
-                            if (entities.includes(fileInputAsJSON[i].Source)) entities.push(JSON.parse(fileInputAsJSON[i].Source));
+                    for (let i = 0; i < this.fileInputAsJSON.length; i++) {
+                        if (!this.entities.includes(this.fileInputAsJSON[i].Target)) this.entities.push(this.fileInputAsJSON[i].Target);
+                        if (!this.entities.includes(this.fileInputAsJSON[i].Source)) this.entities.push(this.fileInputAsJSON[i].Source);
                     }
-                     */
-                    console.log(entities);
+                    //filters NaN, null, not defined and ''
+                    this.entities = this.entities.filter(el => {
+                        return el
+                    });
+                    //random sample
+                    this.getSample(this.entities, 30);
+
+                    for (let i = 0; i < this.fileInputAsJSON.length; i++) {
+                        if (this.rand.includes(this.fileInputAsJSON[i].Target) && this.rand.includes(this.fileInputAsJSON[i].Source)) {
+                            this.sample[ctx] = this.fileInputAsJSON[i];
+                            ctx++;
+                        }
+                    }
+
+                    //calculate the average weight of the CSV file
+                    let sum = 0;
+                    for (let i = 0; i < this.fileInputAsJSON.length; i++) {
+                        sum = sum + parseFloat(this.fileInputAsJSON[i].Weight);
+                    }
+                    this.avgWeight = sum/this.fileInputAsJSON.length;
                 };
                 reader.readAsText(this.csvFile);
+                ctx = 0;
+                this.infoFlag = false;
+            },
+
+            getSample(array, count) {
+                for (let i = 0; i < count; i++)
+                    this.rand.push(array[Math.floor(Math.random() * array.length)])
+            },
+
+            csvSubmit() {
+
             }
         }
     });
