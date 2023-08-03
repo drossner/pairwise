759dff4cd221298676cb2bcc6a6aaade46bf8b24
Daniel Rossner <daniel.rossner@posteo.de>
2023-08-03 08:48:27 +0200

    updated analyzing pipeline

--- R/valueComp.R
+++ R/valueComp.R
@@ -41,6 +41,8 @@ plot(Rating.x, Rating.y)
 abline(lm(Rating.x ~ Rating.y))
 
 
+# new eval for thesis
+
 norm_values <- read.csv("R/src/postcsv.csv", sep = ",", fileEncoding = "UTF-8")
 
 qqnorm(norm_values$spat)
@@ -54,3 +56,30 @@ density(norm_values$comp)
 plot(x=norm_values$spat, y=norm_values$comp)
 
 cor(norm_values$spat, norm_values$comp)
+cor.test(norm_values$spat, norm_values$comp)
+
+# inter rater aggrement
+library("irr")
+comp_values <- read.csv("R/csv-export/Comparison_Information.csv", sep = ",", fileEncoding = "UTF-8")
+
+ratings <- comp_values %>%
+  group_by(ConceptA, ConceptB) %>%
+  arrange(SessionID) %>%
+  #no clue how do that dynamically.. :(
+  summarise(Weight_Mean = mean(Weight), n = n(), rater1 = nth(Weight, 1), rater2 = nth(Weight, 2), rater3 = nth(Weight, 3), rater4 = nth(Weight, 4))
+
+cleaned_ratings <- ratings %>% select(contains("rater"))
+
+icc(
+  cleaned_ratings[,-1], model = "twoway",
+  type = "agreement", unit = "single"
+)
+
+# Koo and Li (2016) gives the following suggestion for interpreting ICC (Koo and Li 2016):
+
+# below 0.50: poor
+# between 0.50 and 0.75: moderate
+# between 0.75 and 0.90: good
+# above 0.90: excellent
+
+
